bbf8ff0b838e7028204d9d2b69bd4b8dad7826d1
bk <kauan.guesser@smartnx.com>
2023-02-05 03:38:05 -0300

    feat: add contracts routes

--- src/app.js
+++ src/app.js
@@ -4,21 +4,40 @@ const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile');
 const { errorHandler } = require('./middleware/errorHandler');
 const { NotFoundException } = require('./utils/errors');
+const { getAllNonTerminatedContracts, getContractById } = require('./repositories/contracts');
+
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+app.get('/contracts/:id', getProfile , async (req, res, next) =>{
+    try {
+    const { id } = req.params
+    const { id: profileId } = req.profile
+
+    const contract = await getContractById(id, profileId)
+   
+    if(!contract) throw new NotFoundException('No contract found')
+    
     res.json(contract)
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.get('/contracts', getProfile , async (req, res, next) =>{
+    try {
+    const { id: profileId } = req.profile
+
+    const contracts = await getAllNonTerminatedContracts(profileId)
+   
+    if(!contracts) throw new NotFoundException('No contract found')
+    
+    res.json(contracts)
+  } catch (error) {
+        next(error)
+    }
 })
 
 app.use(() => { 
---
+++ src/repositories/contracts.js
@@ -0,0 +1,13 @@
+const { Op } = require('sequelize');
+const { Contract } = require("../model");
+
+const getContractById = (id, profileId) => Contract.findOne({ where: { id, [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }] }})
+
+const getAllNonTerminatedContracts = (profileId) => Contract.findAll({
+    where: {
+        status: {[Op.ne]: 'terminated'},
+         [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }]
+        }
+})
+
+module.exports = { getContractById, getAllNonTerminatedContracts }
