be028095900c4d9f3bcf67584f4d6c6bd73dfc83
bk <kauan.guesser@smartnx.com>
2023-02-05 04:58:59 -0300

    feat: add jobs

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ const { errorHandler } = require('./middleware/errorHandler')
 const { NotFoundException, BadRequestException } = require('./utils/errors')
 const { getAllNonTerminatedContracts, getContractById } = require('./repositories/contracts')
 const { findProfile, updateProfileBalance, BALANCE_OPERATION } = require('./repositories/profile')
-const { getTotalJobsToPay } = require('./repositories/jobs')
-
+const { getTotalJobsToPay, getUnpaidJobs, setJobToPaid, getJob } = require('./repositories/jobs')
 
 const app = express()
 app.use(bodyParser.json())
@@ -38,6 +37,49 @@ app.get('/contracts', getProfile, async (req, res, next) => {
 		if (!contracts) throw new NotFoundException('No contract found')
 
 		res.json(contracts)
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.get('/jobs/unpaid', getProfile, async (req,res, next) => {
+	try {
+    
+		const { id: profileId } = req.profile
+
+		const unpaidJobs = await getUnpaidJobs(profileId)
+
+		if(!unpaidJobs) throw new NotFoundException('No unpaid jobs found')
+
+		res.json(unpaidJobs)
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.post('/jobs/:job_id/pay', getProfile, async (req,res, next) => {
+	try {
+		const { id: profileId } = req.profile
+		const { job_id: jobId } = req.params
+
+		if(!jobId) throw BadRequestException()
+
+		const job = await getJob(jobId, profileId)
+
+		if(!job) throw new NotFoundException('No job available to pay')
+
+		const { price, Contract: { Client, Contractor }} = job
+
+		if(price > Client.balance) throw new BadRequestException('You cant pay for this job')
+
+		await Promise.all([
+			updateProfileBalance(Client.id, { value: price, operation: BALANCE_OPERATION.D }),
+			updateProfileBalance(Contractor.id, { value: price, operation: BALANCE_OPERATION.C }),
+			setJobToPaid(jobId)
+		])
+
+		res.json(job)
+    
 	} catch (error) {
 		next(error)
 	}
--- src/repositories/jobs.js
+++ src/repositories/jobs.js
@@ -1,10 +1,53 @@
 const { Op } = require('sequelize')
-const { Job, Contract } = require('../model')
+const { Job, Contract, Profile } = require('../model')
 
-const getTotalJobsToPay = (ClientId) => Job.sum('price', {where: {paid: {[Op.not]: true}}, include: {
-	model: Contract,
-	required: true,
-	where: {ClientId, status: {[Op.ne]: 'terminated'}}
-}})
+const getTotalJobsToPay = (ClientId) => Job.sum('price', {
+	where: {paid: {[Op.not]: true}}, include: {
+		model: Contract,
+		required: true,
+		where: {ClientId, status: {[Op.ne]: 'terminated'}}
+	}})
 
-module.exports = {getTotalJobsToPay}
+const getUnpaidJobs = (profileId) => Job.findAll({
+	where: {
+		paid: {
+			[Op.is]: null}, paymentDate: {[Op.is]: null}  
+	},
+	include: {
+		model: Contract,
+		as: 'Contract',
+		where: {
+			status: {
+				[Op.eq]: 'in_progress',
+			},
+			[Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }],
+		},
+	},
+})
+
+const getJob = (id, profileId) => Job.findOne({ 
+	where: { id, paid: {
+		[Op.is]: null}, paymentDate: {[Op.is]: null}  }, include: {
+		model: Contract,
+		required: true,
+		where: {
+			[Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }]
+		},
+		include: [{
+			model: Profile,
+			as: 'Client',
+			required: true,
+		},
+		{
+			model: Profile,
+			as: 'Contractor',
+			required: true,
+		},
+		]
+	}
+}
+)
+
+const setJobToPaid = (id) => Job.update({ paid: true, paymentDate: new Date() }, { where: { id } })
+
+module.exports = { getTotalJobsToPay, getUnpaidJobs, getJob, setJobToPaid }
